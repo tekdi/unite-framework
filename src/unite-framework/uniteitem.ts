de8cfbca4a7a9b5563bb522d70ee09b652b46451
Vivek kasture <vivek_kasture@techjoomla.com>
2017-05-06 14:47:21 +0530

    Unite comapatibility with ionic 3.0.1

--- src/unite-framework/uniteitem.ts
+++ src/unite-framework/uniteitem.ts
@@ -1,59 +1,155 @@
-
-import {Injectable, Inject} from '@angular/core';
-import {Loading} from 'ionic-angular';
-import {Http, Headers} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Loading, LoadingController, ToastController, Events } from 'ionic-angular';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { TranslateService } from 'ng2-translate';
+import { Network } from '@ionic-native/network';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 
 export class UniteItem {
-	http: Http;
 	loader: Loading;
-	loaderconfig: any;
-	
-	constructor(http: Http) {		
-		this.loaderconfig = { content: "Please wait...", dismissOnPageChange: false };
-		this.http = http;
-	}
+	local: any;
+	loaderOptions: any;
+	toast: any;
+	toastOptions: any;
+	items: any = [];
+	message: any = '';
+	isInfinite: boolean = false;
+	limitstart: number = 0;
+	limit: number = 10;
+	enableifinitescroll: boolean = false;
+	currentLang: string = 'en';
+	siteConfig: any = [];
 
+	constructor(private network: Network, public http: Http, public storage: Storage, public translate: TranslateService, public loadingCtrl: LoadingController, public toastCtrl: ToastController, public events: Events) {
+
+		this.translate.get('label.PLEASE_WAIT').subscribe(value => {
+			this.loaderOptions = { content: value, duration: 10000, spinner: "dots" };
+			this.loader = this.loadingCtrl.create(this.loaderOptions);
+		});
+
+		this.translate.get('message.SOMETHING_WENT_WRONG').subscribe(value => {
+			this.toastOptions = { content: value, duration: 3000, spinner: "dots", position: "middle" };
+			this.toast = this.toastCtrl.create(this.toastOptions);
+		});
+		this.getApiParams();
+		this.events.subscribe('user:created', (res) => {
+			this.getApiParams();
+		});
+	}
 	getData(url) {
-		return this.http.get(url).then((value) => {
-			if (value) {
-				return new Promise(resolve => {
-					return resolve(JSON.parse(value));
-				});
-			} else {
-				return new Promise(resolve => {
-					this.http.get(url)
+		url += '&key=' + this.siteConfig['authkey'] + '&lang=' + this.currentLang;
+		this.items = (this.isInfinite) ? this.items : [];
+		if (this.network.type !== 'none') {
+			return new Promise(resolve => {
+				this.http.get(url)
 					.map(res => res.json())
 					.subscribe(
-					data => {						
+					data => {
+						this.items = this.items.concat(data.data.results);
+						this.message = data.data.empty_message;
 						resolve(data);
-					}, err => {
-						resolve('Error');
+						this.isInfinite = false;
+					},
+					err => {
+						this.toast.present();
+						this.isInfinite = false;
 					});
-				});
+			});
+		} else {
+			this.noNetwork();
+		}
+	}
+
+	postData(posturl, data) {
+		posturl = (this.siteConfig != null) ? posturl + '&key=' + this.siteConfig['authkey'] + '&lang=' + this.currentLang : posturl;
+		if (this.network.type !== 'none') {
+			let url = posturl;
+			let datatobepost = data;
+			let headers = new Headers();
+			headers.append('Content-Type', 'application/x-www-form-urlencoded');
+			return this.http.post(url, datatobepost, {
+				headers: headers
+			}).map(res => res.json());
+		} else {
+			this.noNetwork();
+		}
+	}
+
+	deleteData(posturl) {
+		posturl += '&key=' + this.siteConfig['authkey'] + '&lang=' + this.currentLang;
+		if (this.network.type !== 'none') {
+			let headers = new Headers();
+			headers.append('Content-Type', 'application/x-www-form-urlencoded');
+			return this.http.delete(posturl, {
+				headers: headers
+			}).map(res => res.json());
+		} else {
+			this.noNetwork();
+		}
+	}
+
+	scroll(event, url) {
+		this.isInfinite = true;
+		this.getData(url).then((value: any) => {
+			if (event) {
+				event.complete();
+
+				if (value === undefined || value.data.results == 0) {
+					this.enableifinitescroll = false;
+				} else {
+					this.enableifinitescroll = true;
+				}
 			}
 		});
 	}
-	
-	postData(posturl, data) {
-		let url = posturl;
-		let datatobepost = data;
-		let headers = new Headers();
-		headers.append('Content-Type', 'application/x-www-form-urlencoded');
-		return new Promise(resolve => {
-			this.http.post(url, datatobepost, {
-				headers: headers
-			})
-			.map(res => res.json())				
-			.subscribe(
-			data => {					
-				resolve(data);
-			},
-			err => {
-				resolve('Error');
+
+	showLoader() {
+		this.translate.get('label.PLEASE_WAIT').subscribe(value => {
+			let loading = this.loadingCtrl.create({
+				content: value, spinner: "dots"
 			});
+			loading.present();
+			if (this.items != []) {
+				setTimeout(() => {
+					loading.dismiss();
+				}, 1000);
+			}
 		});
+
+	}
+
+	noNetwork() {
+		this.translate.get('message.NO_INTERNET_CONNECTION').subscribe(value => {
+			this.toastOptions = { message: value, duration: 3000 };
+			this.toast = this.toastCtrl.create(this.toastOptions);
+			this.toast.present();
+		});
+	}
+
+	presentLoading() {
+		this.translate.get('label.PLEASE_WAIT').subscribe(value => {
+			let loader = this.loadingCtrl.create({
+				content: value, duration: 3000
+			});
+			loader.present();
+		});
+	}
+
+	getApiParams() {
+		this.storage.get("siteConfig").then((value) => {
+			this.siteConfig = value;
+		});
+		this.storage.get("appLanguage").then((value) => {
+			this.currentLang = value;
+		});
+	}
+
+	showToastermessage(message) {
+		this.toastOptions.message = message;
+		this.toast = this.toastCtrl.create(this.toastOptions);
+		this.toast.present();
 	}
 }
--- src/unite-framework/unitelist.ts
+++ src/unite-framework/unitelist.ts
@@ -1,87 +1,130 @@
-
-import {Injectable, Inject} from '@angular/core';
-import {LoadingController, NavParams, Toast, NavController} from 'ionic-angular';
-import {Http, Headers} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { LoadingController, ToastController, Events } from 'ionic-angular';
+import { Http } from '@angular/http';
+//import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import {UniteToast} from '../unite-framework/unitetoast';
+import { TranslateService } from 'ng2-translate';
+import { Network } from '@ionic-native/network';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 
 export class UniteList {
-	baseurl: string;
-	http: any;
-	search: string;
-	idname: string;
-	id: number;
-	limitstart: number;
-	limit: number;
-	lastUpdated: string;
-	limitName: string;
-	limitstartName: string;
-	searchName: string;
-	lastUpdatedName: string;
-	loader: any;
-	loaderconfig: any;
+	items:					any;
+	unitelist:				any;
+	url:					string;
+	enableSearchbar:		boolean;
+	enablePullToRefresh:	boolean;
+	enableifinitescroll:	boolean	=	false;
+	noitem:					boolean;
+	infinitescroll:			any;
+	loader:					any;
+	loaderOptions:			any;
+	toast:					any;
+	toastOptions:			any;
+	dataList:				any;
+	limitstart:				number	=	0;
+	limit:					number	=	10;
+	message:				string;
+	isInfinite:				boolean	=	false;
+	currentLang:			string	=	'en'; 
+	siteConfig:				any		=	[];
+
+	constructor(private network: Network,public http: Http, public storage: Storage, public loadingCtrl: LoadingController, public toastCtrl: ToastController, public translate: TranslateService, public events: Events) {
+
+		this.enableSearchbar		=	true;
+		this.enablePullToRefresh	=	true;
+		this.enableifinitescroll	=	false;
+		this.infinitescroll			=	false;
+		this.noitem					=	true;
+		this.limitstart				=	0;
+		this.limit					=	10;
+		this.items					=	[];
+		this.message				=	'';
 		
-	constructor(http: Http, private unitetoast: UniteToast, public loadingCtrl: LoadingController) {			
-		this.limit = 20;
-		this.limitstart = 0;
-		this.limitName = 'limit';
-		this.limitstartName = 'limitstart';
-		this.searchName = 'search';
-		this.idname = "category_id";
-		this.lastUpdatedName = 'last_updated';
-		this.loaderconfig = { content: "Please wait...", dismissOnPageChange: false };
-		this.http = http;
-		this.baseurl = '';
+
+		this.translate.get('label.PLEASE_WAIT').subscribe(value => { 
+				this.loaderOptions	=	{ content: value, duration: 8000, spinner: "dots", dismissOnPageChange: true };
+				this.loader			=	this.loadingCtrl.create(this.loaderOptions);
+		});
+
+		this.translate.get('message.SOMETHING_WENT_WRONG').subscribe(value => { 
+				this.toastOptions	=	{ message: value, duration: 3000 };
+				this.toast			=	this.toastCtrl.create(this.toastOptions);
+			});
+		this.getApiParams();
+		this.events.subscribe('user:created', (res) => { 
+			this.getApiParams();
+		});
 	}
-	
-	getData() {
-		let callingurl = this.baseurl;
-		this.showLoader();
-		if (this.limit) {
-			callingurl += '&' + this.limitName + '=' + this.limit;
-		}
 
-		if (this.limitstart >= 0) {
-			callingurl += '&' + this.limitstartName + '=' + this.limitstart;
-		}
-
-		if (this.search) {
-			callingurl += '&' + this.searchName + '=' + this.search;
-		}
-		
-		if (this.id) {
-			callingurl += '&' + this.idname + '=' + this.id;
-		}
-		
-		if (this.lastUpdated) {
-			callingurl += '&' + this.lastUpdatedName + '=' + this.lastUpdated;
-		}		
-		
-		return new Promise(resolve => {
-			this.http.get(callingurl)
-				.map(res => res.json())
-				.subscribe(
-					data => {							
-						this.limitstart += this.limit;
-						resolve(data);							
-						this.hideLoader();
-					}, err => {
-						resolve('Error');
-						this.hideLoader();
+	getData(url) {
+		url += '&key=' + this.siteConfig['authkey'] + '&lang=' + this.currentLang;
+		this.items	=	(this.isInfinite)?this.items:[];
+		if(this.network.type !== 'none') {
+			return new Promise(resolve => {
+				this.http.get(url)
+					.map(res => res.json())
+					.subscribe(
+					data => {
+						this.items		=	this.items.concat(data.data.results);
+						this.message	=	data.data.empty_message;
+						resolve(data);
+						this.isInfinite	=	false;
+					},
+					err => {
+					//this.hideLoader();
+					this.toast.present();
+					this.isInfinite	=	false;
 					});
-		});			
+			});
+		} else {
+			this.noNetwork();
+		}
 	}
-	
+
+	scroll(event,url){
+		this.isInfinite	=	true;
+		this.getData(url).then((value: any) => {
+			if (event) {
+				event.complete();
+				if (value === undefined || value.data.results == 0) {
+					this.enableifinitescroll	=	false;
+				} else {
+					this.enableifinitescroll	=	true;
+				}
+			}
+		});
+	}
 	showLoader() {
-		this.loader = this.loadingCtrl.create(this.loaderconfig);
-		this.loader.present();
+		this.translate.get('label.PLEASE_WAIT').subscribe(value => { 
+			let loading = this.loadingCtrl.create({
+				content: value, spinner: "dots"
+			});
+			loading.present();
+			if(this.items != []){
+				setTimeout(() => {
+					loading.dismiss();
+				}, 2000);
+			}
+		});
+		
 	}
-	
-	hideLoader() {
-		setTimeout(() => {
-			this.loader.dismiss();
-		})
+
+	noNetwork(){
+		this.translate.get('message.NO_INTERNET_CONNECTION').subscribe(value => { 
+			this.toastOptions = { message: value, duration: 3000 };
+			this.toast = this.toastCtrl.create(this.toastOptions);
+			this.toast.present();
+		});
+	}
+
+	getApiParams(){
+		this.storage.get("siteConfig").then((value) => {
+			this.siteConfig	=	value;
+		});
+		this.storage.get("appLanguage").then((value) => {
+			this.currentLang = value;
+		});
 	}
 }

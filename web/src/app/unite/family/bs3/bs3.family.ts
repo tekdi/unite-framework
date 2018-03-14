be5717c8f5b9dde8d16e65b8d6f32814c887e293
“Ranjit <ranjit_r@techjoomla.com>
2018-03-14 16:01:25 +0530

    Change BS4 family to BS3 family

--- web/src/app/unite/family/bs3/bs3.family.ts
+++ web/src/app/unite/family/bs3/bs3.family.ts
@@ -5,37 +5,37 @@ import {RouterModule, Routes, ActivatedRoute} from '@angular/router'
 import { TemplateSelector } from '../../uniteDirectives/template.selector';
 import { RendererSelector } from '../../uniteDirectives/renderer.selector';
 import { UniteLinkDirective } from '../../uniteDirectives/makelink.directive';
-import { bs4Comp } from './bs4.component';
+import { bs3Comp } from './bs3.component';
 
 import { UniteRouting } from '../../uniteServices/routingService';
 
-import { bs4Templates } from './templates/templates.collections';
-import { bs4Renderers } from './renderer/renderers.collection';
+import { bs3Templates } from './templates/templates.collections';
+import { bs3Renderers } from './renderer/renderers.collection';
 
 import { UniteMapperPipe } from '../../pipes/mapper.pipe';
 import { UniteLinkerPipe } from '../../pipes/linker.pipe';
 
 const r : Routes = [ 
-    {path : "**", component : bs4Comp} 
+    {path : "**", component : bs3Comp} 
     ];
 
 @NgModule({
     imports : [CommonModule, RouterModule.forChild(r)],
     declarations : [
-                    bs4Comp,
-                    bs4Templates,
-                    bs4Renderers,
+                    bs3Comp,
+                    bs3Templates,
+                    bs3Renderers,
                     TemplateSelector,
                     RendererSelector,
                     UniteMapperPipe,
                     UniteLinkerPipe,
                     UniteLinkDirective
                     ],
-    entryComponents : [bs4Templates, bs4Renderers],
+    entryComponents : [bs3Templates, bs3Renderers],
     providers : []
 })
-export class bs4Family{
+export class bs3Family{
     constructor(){
-        console.log("this is BS4 family....");
+        console.log("this is bs3 family....");
     }
 }

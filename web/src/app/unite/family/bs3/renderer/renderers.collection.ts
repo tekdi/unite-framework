e148035265c2c40f8a516577765136b8b28fd1de
Sudhir <sudhir_s@tekditechnologies.com>
2018-03-15 16:03:58 +0530

    Footer, Sites, Page Counter, Diksha in News Widgets

--- web/src/app/unite/family/bs3/renderer/renderers.collection.ts
+++ web/src/app/unite/family/bs3/renderer/renderers.collection.ts
@@ -1,12 +1,15 @@
 import { CustomRenderer } from './custom/renderer';
 import { CarouselRenderer } from './carousel/renderer';
+import { UnorderedRenderer } from './list/unordered/renderer';
 
 export const renderMapper = {
         custom: CustomRenderer,
-        carousel : CarouselRenderer
+        carousel : CarouselRenderer,
+        unorderedlist : UnorderedRenderer
 }
 
 export const bs3Renderers = [
                                 CustomRenderer,
-                                CarouselRenderer
+                                CarouselRenderer,
+                                UnorderedRenderer
                             ]

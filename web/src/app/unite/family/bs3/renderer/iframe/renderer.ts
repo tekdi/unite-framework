a04dddb3dfaf8a8982b8c5290b70157dc4ec36e1
Sudhir <sudhir_s@tekditechnologies.com>
2018-03-20 14:51:46 +0530

    Task #123310 You tube video frame loading in About us widget

---
+++ web/src/app/unite/family/bs3/renderer/iframe/renderer.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+    templateUrl : "./renderer.html",
+})
+export class IframeRenderer{
+    @Input() data;
+    @Input() widName;
+    @Input() set mapper(value){
+        this.mapProperties(this.data, value);
+    };
+
+    constructor (private sanitizer:DomSanitizer){
+
+    }
+    localData;
+    localMap;
+
+    mapProperties(data, mapObj) {
+        this.localMap = mapObj;
+        this.localData = data;
+        this.localData.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.data.url);
+    }
+
+    
+}
--- web/src/app/unite/family/bs3/renderer/renderers.collection.ts
+++ web/src/app/unite/family/bs3/renderer/renderers.collection.ts
@@ -2,17 +2,20 @@ import { StatesmapRenderer } from './statesmap/renderer';
 import { CustomRenderer } from './custom/renderer';
 import { CarouselRenderer } from './carousel/renderer';
 import { UnorderedRenderer } from './list/unordered/renderer';
+import { IframeRenderer } from './iframe/renderer';
 
 export const renderMapper = {
         custom: CustomRenderer,
         carousel : CarouselRenderer,
         unorderedlist : UnorderedRenderer,
-        statesmap : StatesmapRenderer
+        statesmap : StatesmapRenderer,
+        iframe : IframeRenderer
 }
 
 export const bs3Renderers = [
                                 CustomRenderer,
                                 CarouselRenderer,
                                 UnorderedRenderer,
-                                StatesmapRenderer
+                                StatesmapRenderer,
+                                IframeRenderer
                             ]

c34f45150c9bf435a1332722446510591afd88bc
“Ranjit <ranjit_r@techjoomla.com>
2018-04-05 18:45:20 +0530

    DATASOURCE REMOVE

--- web/src/app/unite/uniteServices/routingService.ts
+++ web/src/app/unite/uniteServices/routingService.ts
@@ -1,48 +1,24 @@
 import { Injectable } from "@angular/core";
 import { MenusService, WidgetsService } from "../core/services";
-import { Config, Menu } from './../core/classes';
+import { Menu } from './../core/classes';
 
 @Injectable()
 export class UniteRouting {
-    menu;
-    menus;
+    public menu;
+    public menus;
+
     constructor(
         private _menusService: MenusService,
         private _widgetsService: WidgetsService,
-        private _config: Config,
-        private _menu: Menu) 
-    {
-        debugger;
-        console.log("NEW MENU INSTANCE AT ROUTING 1", this._menu);
-        console.log("NEW MENU INSTANCE AT ROUTING 2", this._menu.getInstance());
-    }
+        private _menu: Menu) { }
 
-    getMenus() {
+    public getMenus() {
         this.menus = this._menu.getMenus();
         this.menu = this._menu.getInstance();
         this.getMenuWidgets();
     }
-/*
-    getAllMenus() {
-        let finalUniteBasePath = "";
-        let menusToReturn = [];
 
-        finalUniteBasePath += this._config.baserUnitePath['basePath'] ? this._config.baserUnitePath['basePath'] + "/" : "";
-        finalUniteBasePath += this._config.baserFamilyPath['basePath'] ? this._config.baserFamilyPath['basePath'] + "/" : '';
-        console.log("FINAL UNITE BASE PATH", finalUniteBasePath);
-        console.log('%c CLASS CONFIG', 'color: green; font-weight: bold;', this._config);
-        this.finalMenus.forEach(element => {
-            let thisElement = element;
-            if (thisElement.menuUrl.indexOf(':') == -1) {
-                thisElement.menuUrl = finalUniteBasePath + thisElement.menuUrl;
-                menusToReturn.push(thisElement);
-            }
-        });
-        console.log(menusToReturn);
-        return menusToReturn;
-    }
-*/
-    getMenuWidgets() {
+    public getMenuWidgets() {
         console.log("GET ALL WIDGETS MENU", this.menu);
         this._widgetsService.getWidgets(this.menu.menuUrl).subscribe(widgets => {
             this.menu.widgets = widgets;

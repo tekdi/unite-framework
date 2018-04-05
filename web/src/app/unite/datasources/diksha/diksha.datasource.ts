801a206222db11b13179f4de74b7896006ecf33b
“Ranjit <ranjit_r@techjoomla.com>
2018-04-05 18:24:50 +0530

    DATASOURCE REMOVE

--- web/src/app/unite/datasources/diksha/diksha.datasource.ts
+++ web/src/app/unite/datasources/diksha/diksha.datasource.ts
@@ -22,13 +22,4 @@ export class DikshaDataSource {
             return asdf;
         });
     }
-
-    // setRoutes(baseSegment) {
-    //     let myRouteObj = [
-    //         { title: "Home", path: "", id: "5aa7c0f34716eb7d786ef1b1" },
-    //         { title: "Partners", path: "partners", id: "5aa7c0f34716eb7d786ef1b2" },
-    //         { title: "Contents", path: "contents", id: "5ab1fd7331faf2397199ac18" }
-    //     ]
-    //     return myRouteObj;
-    // }
 }

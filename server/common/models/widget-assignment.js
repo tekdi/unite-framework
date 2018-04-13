66ec72fa55de2775b9887929251553b2e8fc687e
Sudhir <sudhir_s@tekditechnologies.com>
2018-04-13 19:39:47 +0530

    Feature #124214 feat: Widget Positioning Improvements and Widget Ordering in the Respective Sections of template

--- server/common/models/widget-assignment.js
+++ server/common/models/widget-assignment.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Widgetassignment) {
-
+    Widgetassignment.afterRemote('**', function(ctx, Widgetassignment, next) {
+        var answers = {};
+        if (ctx.result) {
+            if (Array.isArray(ctx.result)) {
+                var positions = {},i, j, currrentElement;
+                var result = ctx.result;
+                for(i = 0, j = result.length; i < j; i++){
+                    currrentElement = result[i];
+                    if (!(currrentElement.position in positions)) {
+                        positions[currrentElement.position] = { widgets: []};
+                        answers[currrentElement.position] = positions[currrentElement.position];                     
+                    }
+                    positions[currrentElement.position].widgets.push(currrentElement);
+                }
+            } else {
+                answers = result;
+            }
+            ctx.result = answers;
+        }
+        next();
+    });
 };
+

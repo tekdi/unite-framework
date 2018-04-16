fbef8e91b3cbb5eca52f29ce925baa975bfa7364
Sudhir <sudhir_s@tekditechnologies.com>
2018-04-16 16:57:06 +0530

    Widget Name not showing after some api change

--- web/src/app/unite/core/directives/renderer.selector.ts
+++ web/src/app/unite/core/directives/renderer.selector.ts
@@ -70,7 +70,6 @@ import { dataSources } from '../../datasources/sources.collection';
     }
 
     loadData(widgetInfo, thisCompRef){
-   
         let config = {
             urlData: widgetInfo.param ? widgetInfo.param : {},
             config: widgetInfo.config ? widgetInfo.config : {}
@@ -129,7 +128,7 @@ import { dataSources } from '../../datasources/sources.collection';
     setDynamicComponentInputs(widgetInfo, thisCompRef, metadata, data) {
         (thisCompRef.instance).data = data;
         (thisCompRef.instance).mapper = widgetInfo.mapper ? widgetInfo.mapper : {};
-        (thisCompRef.instance).widgetName = widgetInfo.widgetName;
+        (thisCompRef.instance).widgetName = widgetInfo.name;
         (thisCompRef.instance).metadata = metadata;
         this.dynamicComponents.push(thisCompRef);
     }
